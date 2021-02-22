f89f8a1bcdad844f3a45d5fc5b90bb655d8670f5
Julien Az <julien.azbrg@gmail.com>
2021-02-22 20:42:52 +0100

    added

---
+++ precache-manifest.11f2edbdb41843d84d25a07d7ee8ee8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4324461d07b116dc7005ba3fbd48747d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0ec8461a1c8e92761e",
+    "url": "/static/css/main.41cbf97a.chunk.css"
+  },
+  {
+    "revision": "31f49453f9a308746860",
+    "url": "/static/js/2.318b6ee5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.318b6ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0ec8461a1c8e92761e",
+    "url": "/static/js/main.89ec941f.chunk.js"
+  },
+  {
+    "revision": "dff33d66bb29ba4d820f",
+    "url": "/static/js/runtime-main.62d92f4f.js"
+  },
+  {
+    "revision": "905c77bf2b36d23605a1823e48c469d9",
+    "url": "/static/media/Michroma-Regular.905c77bf.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
